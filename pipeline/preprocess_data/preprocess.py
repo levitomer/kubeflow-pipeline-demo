3df3a8e91b57db8d35183a2f51575fc42eb58387
Tomer Levi <18260079+levitomer@users.noreply.github.com>
2022-06-02 16:14:33 +0300

    log exception for pre-process

--- pipeline/preprocess_data/preprocess.py
+++ pipeline/preprocess_data/preprocess.py
@@ -3,12 +3,15 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 def _preprocess_data():
-     X, y = datasets.fetch_california_housing(return_X_y=True)
-     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-     np.save('x_train.npy', X_train)
-     np.save('x_test.npy', X_test)
-     np.save('y_train.npy', y_train)
-     np.save('y_test.npy', y_test)
+     try:
+
+          X, y = datasets.fetch_california_housing(return_X_y=True)
+          X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+          np.save('x_train.npy', X_train)
+          np.save('x_test.npy', X_test)
+          np.save('y_train.npy', y_train)
+          np.save('y_test.npy', y_test)
+     except Exception as e: print(e)
 
 if __name__ == '__main__':
      print('Preprocessing data...')
